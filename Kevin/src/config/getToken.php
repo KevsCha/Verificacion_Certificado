bbfc7f36ddde2f4960b0f873e5aa6b059f3a4e18
Kevin Quispe Carrillo <107918168+KevsCha@users.noreply.github.com>
2025-05-30 13:02:00 +0200

    eleven commit: getToken update file

--- Kevin/src/config/getToken.php
+++ Kevin/src/config/getToken.php
@@ -2,3 +2,10 @@
 function getToken() {
     return json_decode(file_get_contents(__DIR__ . '/token.json'), true);
 }
+function tokenInvalid($token):bool {
+    $tiempoActual = time(); // Timestamp actual en segundos
+    $expiracion = (int)$token['expires'];
+
+    // Margen de 1 minuto por seguridad
+    return ($expiracion - 60) < $tiempoActual;
+}
